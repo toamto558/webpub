a3643cffc061c6ec35da388366e9373bd3e62b9b
toamto558 <110583197+toamto558@users.noreply.github.com>
2022-08-04 15:56:42 +0900

    공개문제

---
+++ 01_end/js/script.js
@@ -0,0 +1,32 @@
+$(".gnb").hover(
+    function(){
+        $(".sub").stop().slideDown();
+    },
+    function(){
+        $(".sub").stop().slideUp();
+    }
+);
+
+$(".tab1").click(
+    function(){
+        $(".notice").show();
+        $(".gallery").hide();
+    }
+)
+$(".tab2").click(
+    function(){
+        $(".notice").hide();
+        // $(".gallery").show();
+        $(".gallery").css("display","flex");
+    }
+);
+$(".open").click(
+    function(){
+        $("#popup").fadeIn();
+    }
+);
+$(".close").click(
+    function(){
+        $("#popup").fadeOut();
+    }
+);
